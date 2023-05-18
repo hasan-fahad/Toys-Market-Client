2d3239f520512a6f2b622e7fbe85c4369f6edb9d
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-05-18 17:06:29 +0600

    navbar

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+
+import logo from '../../../assets/Images/logo.png'
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-slate-300 rounded text-base-content">
+  <div>
+    <img className=" w-36"src={logo} alt="" />
+    <p>Kiddo Baby Shop<br/>Here you found all things for baby</p>
+  </div> 
+  <div>
+    <span className="footer-title">Services</span> 
+    <a className="link link-hover">Branding</a> 
+    <a className="link link-hover">Design</a> 
+    <a className="link link-hover">Marketing</a> 
+    <a className="link link-hover">Advertisement</a>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </div>
+</footer>
+    );
+};
+
+export default Footer;
