ff7585c518f75e259734b9e9f8412ec60218b2b2
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-05-18 17:39:52 +0600

    anchor modified

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -4,26 +4,26 @@ import img2 from '../../assets/Images/banner2.jpg'
 import img3 from '../../assets/Images/banner3.jpg.webp'
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full rounded-lg">
   <div id="slide1" className="carousel-item relative w-full">
     <img src={img1} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
+    <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide3" className="btn btn-outline btn-primary btn-circle mx-3">❮</a> 
+      <a href="#slide2" className="btn btn-outline btn-primary btn-circle mx-3">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
     <img src={img2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
+    <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide1" className="btn btn-outline btn-primary btn-circle mx-3">❮</a> 
+      <a href="#slide3" className="btn btn-outline btn-primary btn-circle mx-3">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
     <img src={img3} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
+    <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide2" className="btn btn-outline btn-primary btn-circle mx-3">❮</a> 
+      <a href="#slide3" className="btn btn-outline btn-primary btn-circle mx-3">❯</a>
     </div>
   </div> 
 </div>
