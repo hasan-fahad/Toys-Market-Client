6a08183714372319ce038ff0d32a69503f176f65
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-05-18 17:53:44 +0600

    banner modified

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -4,13 +4,18 @@ import img2 from '../../assets/Images/banner2.jpg'
 import img3 from '../../assets/Images/banner3.jpg.webp'
 const Banner = () => {
     return (
-        <div className="carousel w-full rounded-lg">
+        <div className="carousel w-full rounded-lg h-[400px]">
   <div id="slide1" className="carousel-item relative w-full">
     <img src={img1} className="w-full" />
     <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0">
       <a href="#slide3" className="btn btn-outline btn-primary btn-circle mx-3">❮</a> 
       <a href="#slide2" className="btn btn-outline btn-primary btn-circle mx-3">❯</a>
     </div>
+    <div className="absolute flex transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div>
+      <h1 className='text-black text-6xl font-bold'>Make Your Children Happy <br /> By Buying Toys</h1>
+      </div>
+      </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
     <img src={img2} className="w-full" />
@@ -18,6 +23,11 @@ const Banner = () => {
       <a href="#slide1" className="btn btn-outline btn-primary btn-circle mx-3">❮</a> 
       <a href="#slide3" className="btn btn-outline btn-primary btn-circle mx-3">❯</a>
     </div>
+    <div className="absolute flex transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div>
+      <h1 className='text-black text-6xl font-bold'>Make Your Children Happy <br /> By Buying Toys</h1>
+      </div>
+      </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
     <img src={img3} className="w-full" />
@@ -25,6 +35,11 @@ const Banner = () => {
       <a href="#slide2" className="btn btn-outline btn-primary btn-circle mx-3">❮</a> 
       <a href="#slide3" className="btn btn-outline btn-primary btn-circle mx-3">❯</a>
     </div>
+    <div className="absolute flex transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div>
+      <h1 className='text-black text-6xl font-bold'>Make Your Children Happy <br /> By Buying Toys</h1>
+      </div>
+      </div>
   </div> 
 </div>
     );
